33a31f0200c539c3c29cacfbed8dff85834766b2
sanzharmaidan <sanchom00298@gmail.com>
2019-02-14 11:48:43 +0600

    first try

---
+++ Week 1/Task2/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public int year;
+
+        public Student (string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+        public Student(int year)
+        {
+            this.year = year;
+        }
+        public void Plus()
+        {
+            year++;
+        }
+        
+        public void show()
+        {
+            Console.WriteLine(name + " " + " " + year);
+        }
+    }
+    class Program
+    { 
+        public static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string[]s = a.Split();
+            Student Studik = new Student(s[0], s[1]);
+            Studik.year = int.Parse(s[2]);
+            Studik.Plus();
+
+        }
+    }
+}
---
+++ Week 2/Task1/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void f1()
+        {
+            StreamReader st = new StreamReader("C:/Users/Санжар/Desktop/input.txt");
+            string s = st.ReadToEnd();
+            string[] t = s.Split();
+            int n = t.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (t[i] != t[n - i - 1])
+                {
+                    Console.WriteLine("NO");
+                    return;
+
+                }
+                st.Close();
+                Console.WriteLine("YES");
+                Console.ReadKey();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            f1(); Console.ReadKey();
+        }
+
+    }
+}
+
